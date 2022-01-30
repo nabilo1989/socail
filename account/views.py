df3475150bb4419e069a5bb887e20f36e58957eb
majidnabilo <nabilo.majid@gmail.com>
2022-01-30 18:26:57 +0330

    Validation not correct register

--- account/views.py
+++ account/views.py
@@ -17,6 +17,6 @@ class RegisterView(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(cd['username'],cd['email'],cd['password'])
+            User.objects.create_user(cd['username'],cd['email'],cd['password1'])
             messages.success(request, 'you are registerd succsefullu', 'success')
             return redirect('home:home')
