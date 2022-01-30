dea98531870874a9c6ae823d2294c4e8110b7711
majidnabilo <nabilo.majid@gmail.com>
2022-01-30 20:41:13 +0330

    loguot redirect error

--- account/views.py
+++ account/views.py
@@ -3,12 +3,18 @@ from django.views import View
 from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login,logout
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class UserRegisterView(View):
     form_class = UserRegistrationForm
+    template_name = 'account/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
         form = self.form_class()
@@ -27,6 +33,11 @@ class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'account/login.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
         form = self.form_class
         return render(request, self.template_name, {'form': form})
@@ -37,14 +48,15 @@ class UserLoginView(View):
             cd = form.cleaned_data
             user = authenticate(request, username=cd['username'], password=cd['password'])
             if user is not None:
-                login(request,user)
+                login(request, user)
                 messages.success(request, 'you are log in succsefullu', 'success')
                 return redirect('home:home')
             messages.error(request, 'username or password is wrong', 'warning')
         return render(request, self.template_name, {'form': form})
 
-class UserLogoutView(View):
-    def get(self,request):
+
+class UserLogoutView(LoginRequiredMixin,View):
+    def get(self, request):
         logout(request)
         messages.success(request, 'you are log  out succsefullu', 'success')
         return redirect('home:home')
