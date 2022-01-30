96ba856356e0bde85d8066309f2aac9827d97e2b
majidnabilo <nabilo.majid@gmail.com>
2022-01-30 19:57:09 +0330

    create login Trou

--- account/forms.py
+++ account/forms.py
@@ -18,12 +18,17 @@ class UserRegistrationForm(forms.Form):
         p2=cd.get('password2')
 
         if p1 and p2 and p1 != p2:
-            raise ValidationError('password in not correct')
+            raise ValidationError('password is not correct')
 
-    #
     def clean_email(self):
         email = self.cleaned_data['email']
         user = User.objects.filter(email=email).exists()
         if user:
             raise ValidationError('this email is already exists')
         return email
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Eenter username'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'EenterPassword'}))
