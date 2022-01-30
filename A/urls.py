1e02ede602b59f6ed5a6a589d539d8f51f385c2c
majidnabilo <nabilo.majid@gmail.com>
2022-01-30 13:45:22 +0330

    create name space

--- A/urls.py
+++ A/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('home.urls')),
+    path('', include('home.urls', namespace='home')),
     path('admin/', admin.site.urls),
-    path('account/',include('account.urls'))
+    path('account/', include('account.urls', namespace='account'))
 ]
--- account/forms.py
+++ account/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField()
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Eenter username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control','placeholder': 'Eenter Emali'}))
+    password = forms.CharField(widget=forms.PasswordInput({'class': 'form-control'}))
