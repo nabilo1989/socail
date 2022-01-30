7848cb45c11c4989c76cb321e4dd9252721aa402
majidnabilo <nabilo.majid@gmail.com>
2022-01-30 16:46:34 +0330

    create user registertion with eror

--- account/views.py
+++ account/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Create your views here.
 class RegisterView(View):
+    form_class = UserRegistrationForm
+
     def get(self, request):
-        form = UserRegistrationForm()
+        form = self.form_class()
         return render(request, 'account/register.html', {'form': form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'],cd['email'],cd['password'])
+            messages.success(request, 'you are registerd succsefullu', 'success')
+            return redirect('home:home')
+        # else:
+        #     form = UserRegistrationForm()
+        # return render(request, 'account/register.html', {'form': form})
