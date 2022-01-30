a7d27fb8bd1d9f079dd9c23b9b85dcdec8ce7766
majidnabilo <nabilo.majid@gmail.com>
2022-01-30 20:54:20 +0330

    authenticateEmailBackend

--- A/settings.py
+++ A/settings.py
@@ -118,5 +118,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+AUTHENTICATION_BACKENDS=[
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authenticated.EmailBackend',
+]
 LOGIN_URL = 'account/login'
