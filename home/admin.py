2a0cbb21cc4a71579e23235cb49f729a46e230ea
majidnabilo <nabilo.majid@gmail.com>
2022-01-31 21:36:58 +0330

    comment

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -9,3 +10,8 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('body',)}
     raw_id_fields = ('user',)
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created', 'is_reply')
+    raw_id_fields = ('user', 'post', 'reply')
--- home/models.py
+++ home/models.py
@@ -11,11 +11,25 @@ class Post(models.Model):
     update = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering=('-created',)
+        ordering = ('-created',)
 
     def __str__(self):
         return f'{self.slug}-{self.user}'
 
     def get_absolute_url(self):
         return reverse('home:post_detail', args=(self.id, self.slug))
+
+
 # Create your models here.
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucommeents')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pcommeents')
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='rcomments', blank=True, null=True)
+    is_reply = models.BooleanField(default=False)
+    body = models.TextField(max_length=400)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user}-{self.body[:30]}'
