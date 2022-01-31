652e7dda8b3aac15bd531a25faefa54bbf7595cb
majidnabilo <nabilo.majid@gmail.com>
2022-01-31 18:53:58 +0330

    404 page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import PostUpdateForm
+from .forms import PostCreateUpdateForm
 from django.utils.text import slugify
 
 
@@ -15,13 +15,13 @@ class HomeView(View):
 
 class PostDetailView(View):
     def get(self, request, post_id, post_slug):
-        post = Post.objects.get(id=post_id, slug=post_slug)
+        post = get_object_or_404(Post,pk=post_id,slug=post_slug)
         return render(request, 'home/detail.html', {'post': post})
 
 
 class PostDeleteView(LoginRequiredMixin, View):
     def get(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+        post = get_object_or_404(Post,pk=post_id)
         if post.user.id == request.user.id:
             post.delete()
             messages.success(request, 'Post deleted succsefullu', 'success')
@@ -31,22 +31,26 @@ class PostDeleteView(LoginRequiredMixin, View):
 
 
 class PostUpdateView(LoginRequiredMixin, View):
-    form_class = PostUpdateForm
+    form_class = PostCreateUpdateForm
+
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = get_object_or_404(Post,pk=kwargs['post_id'])
+        return super().setup(request, *args, **kwargs)
 
     def dispatch(self, request, *args, **kwargs):
-        post = Post.objects.get(pk=kwargs['post_id'])
+        post = self.post_instance
         if not post.user.id == request.user.id:
             messages.error(request, 'you arecan not update this post', 'danger')
             return redirect('home:home')
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+    def get(self, request, *args, **kwargs):
+        post = self.post_instance
         form = self.form_class(instance=post)
         return render(request, 'home/update.html', {'form': form})
 
-    def post(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+    def post(self, request, *args, **kwargs):
+        post = self.post_instance
         form = self.form_class(request.POST, instance=post)
         if form.is_valid():
             new_post = form.save(commit=False)
@@ -54,3 +58,22 @@ class PostUpdateView(LoginRequiredMixin, View):
             new_post.save()
             messages.success(request, 'you update post', 'success')
             return redirect('home:post_detail', post.id, post.slug)
+
+
+class PostCreateView(LoginRequiredMixin, View):
+    form_class = PostCreateUpdateForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class
+        return render(request, 'home/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_post=form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.user= request.user
+            new_post.save()
+            messages.success(request, 'you are created a new post', 'success')
+            return redirect('home:post_detail', new_post.id, new_post.slug)
+
