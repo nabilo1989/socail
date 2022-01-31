e25fa317fe65f2ebcb33d320d22eef678b5ea7c7
majidnabilo <nabilo.majid@gmail.com>
2022-01-31 11:56:58 +0330

    show post home.index

--- home/views.py
+++ home/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Post
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/home.html')
+        posts = Post.objects.all()
+        return render(request, 'home/home.html', {'posts': posts})
 
-    def post(self, request):
-        return render(request, 'home/home.html')
+
+class PostDetailView(View):
+    def get(self, request, post_id, post_slug):
+        post = Post.objects.get(pk=post_id, slug=post_slug)
+        return render(request, 'home/detail.html', {'post': post})
